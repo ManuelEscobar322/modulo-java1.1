68ce9171efdbe94bbda7f13dde6d3864997a3826
Manuel <Manuelescobar2287@gmail.com>
2024-08-08 02:01:47 -0500

    Se calcula el módulo

--- ProyectoModulo/src/main/java/com/mycompany/proyectomodulo/ProyectoModulo.java
+++ ProyectoModulo/src/main/java/com/mycompany/proyectomodulo/ProyectoModulo.java
@@ -3,14 +3,22 @@
  */
 
 package com.mycompany.proyectomodulo;
-
+import java.util.*;
 /**
  *
  * @author manol
  */
 public class ProyectoModulo {
+    
+    public static int ProyectoModulo(int n1, int n2){
+        return n1 % n2;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner lector = new Scanner(System.in);
+        int numero1 = lector.nextInt();
+        int numero2 = lector.nextInt();
+        int resultado = ProyectoModulo (numero1, numero2);
+        System.out.println("El resultado del módulo es" + resultado);
     }
 }
